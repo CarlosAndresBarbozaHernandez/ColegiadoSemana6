44cff65ea58198902b740b7613fa8e8ac4034e1b
“Carlos Barboza” <152022250+CarlosAndresBarbozaHernandez@users.noreply.github.com>
2024-02-25 18:53:50 -0600

    Primer commit

---
+++ src/colegiadosemana6/ColegiadoSemana6.java
@@ -0,0 +1,19 @@
+package colegiadosemana6;
+
+/**
+ *
+ * @author Carlos Andres Barboza Hernandez
+ * 
+ */
+public class ColegiadoSemana6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hola mi gente");
+        
+    }
+    
+}
